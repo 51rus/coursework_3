d1b1ab0e667dcd678dc76fc8fc27cda374a6eece
Georgiy <gmar12@yandex.ru>
2022-12-03 10:51:53 +0300

    Bug fix in the template and add logs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from api.api import api_blueprint
 from main.views import main_blueprint
@@ -6,6 +7,8 @@ app = Flask(__name__)
 app.config['JSON_AN_ASCII'] = False
 app.register_blueprint(main_blueprint)
 app.register_blueprint(api_blueprint)
+
+logging.basicConfig(filename='logs/api.log', level=logging.INFO)
 
 if __name__ == '__main__':
     app.run(debug=True)
